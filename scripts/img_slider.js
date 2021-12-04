c38caaf9792686c89a555bfa922060dc648db7bc
Leonardo Antunes <leonardoantunes1401@gmail.com>
2021-12-03 22:13:28 -0300

    adicionou css e js da galeria

---
+++ scripts/img_slider.js
@@ -0,0 +1,42 @@
+const slider = document.querySelector(".slider");
+const imgs = document.querySelectorAll(".slider img");
+
+const prevBtn = document.querySelector("#prev_btn");
+const nextBtn = document.querySelector("#next_btn");
+
+let currentImgIndex = 1;
+
+function slide() {
+    slider.style.transform = `translateX(${ (-imgs[0].clientWidth * currentImgIndex) }px)`;
+}
+
+
+// event listeners
+nextBtn.addEventListener("click", () => {
+    if ( currentImgIndex >= imgs.length -1 ) return;
+
+    currentImgIndex++;
+    slider.style.transition = "transform .5s ease-out";
+    slide();
+});
+
+prevBtn.addEventListener("click", () => {
+    if ( currentImgIndex <= 0 ) return;
+
+    slider.style.transition = "transform .5s ease-out";
+    currentImgIndex--;
+    slide();
+});
+
+slider.addEventListener("transitionend", () => {
+    if (imgs[currentImgIndex].id === "clone_ultima_img") {
+        slider.style.transition = "none";
+        currentImgIndex = imgs.length -2;
+        slide();
+    }
+    if (imgs[currentImgIndex].id === "clone_primeira_img") {
+        slider.style.transition = "none";
+        currentImgIndex = imgs.length - currentImgIndex;
+        slide();
+    }
+});
